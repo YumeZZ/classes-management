367112f86a7800dde3c176ac8d30b46225ed2b0e
yumezz <s104213052@mail1.ncnu.edu.tw>
2019-01-16 13:46:26 +0800

    NCNU PROJECT

---
+++ js/room.js
@@ -0,0 +1,28 @@
+// $(document).ready(function() {
+//     $.ajax({
+//         type:"GET",
+//         url: "https://xn--pss23c41retm.tw/room/",
+//         success: function(data) {
+//             // var result = JSON.parse(data);
+//             // for (var i = 0; i < result.length; i++) {
+//                 console.log(data);
+//                 //     "</td><td class='genus'>"+ result[i].genus +
+//                 //     "</td><td class='species'>"+ result[i].species +
+//                 //     "</td><td class='info'>"+ result[i].info +
+//                 //     "</td><td class='place'>"+ result[i].place +
+//                 //     "</td><td><button class='update' onclick='editOne("+ result[i].id +
+//                 //     ")'>修改</button><button class='deleteOne'"+
+//                 //     " onclick='deleteOne("+ result[i].id +")'>刪除</button>" +
+//                 //     "</td></tr>";
+//                 //     // <button class='update' onclick='editOne("+ result[i].id +
+//                 //     // ")'>修改</button>
+//                 //     // console.log();
+//                 // $(".status").append(data);
+//             // }
+            
+//         },
+//         error: function(data) {
+//             alert("load data fail");
+//         }
+//     });
+// });
